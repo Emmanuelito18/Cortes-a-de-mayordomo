dbac75f095ac58fb0471eafde91e6c29bebcbaba
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-15 18:24:39 -0600

    Commit inicial

---
+++ src/main/java/com/emmanuelito18_code/cortesiademayordomo/CortesiaDeMayordomo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.emmanuelito18_code.cortesiademayordomo;
+
+/**
+ *
+ * @author Emmanuelito18
+ */
+public class CortesiaDeMayordomo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
