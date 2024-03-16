604d6b0673141c31d7c7d557aa64f5e2097daa38
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-15 20:30:42 -0600

    -Cambios en CortesiaDeMayordomo.java *Se eliminaron las importaciones las librerias necesarias para la alexa skill
    -Se creo la clase LaunchRequestHandler.java

--- src/main/java/com/emmanuelito18_code/cortesiademayordomo/CortesiaDeMayordomo.java
+++ src/main/java/com/emmanuelito18_code/cortesiademayordomo/CortesiaDeMayordomo.java
@@ -4,13 +4,6 @@
 
 package com.emmanuelito18_code.cortesiademayordomo;
 
-//Se importan las librerías para las alexa skills
-import com.amazon.ask.dispatcher.request.handler.HandlerInput;
-import com.amazon.ask.dispatcher.request.handler.RequestHandler;
-import com.amazon.ask.model.LaunchRequest;
-import com.amazon.ask.model.Response;
-import com.amazon.ask.request.Predicates;
-
 /**
  *
  * @author Emmanuelito18
