16564ecff858465d202d79b42820ad8dba2581ed
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-15 20:47:33 -0600

    -Cambios en LaunchRequestHandler.java *Se añadieron los imports de las librerias necesarias

--- src/main/java/com/emmanuelito18_code/Handlers/LaunchRequestHandler.java
+++ src/main/java/com/emmanuelito18_code/Handlers/LaunchRequestHandler.java
@@ -4,6 +4,14 @@
  */
 package com.emmanuelito18_code.Handlers;
 
+//Se importan las librerías para las alexa skills
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.LaunchRequest;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.request.Predicates;
+
+import java.util.Optional;
 /**
  * @author Emmanuelito18
  */
