63a90d9c9cb963121356c6dbb8cdf6f033a64fe5
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-16 21:21:08 -0600

    -Cambios en CancelandStopIntentHandler.java *Se importaron las librerias para la alexa skill del CancelandStopIntentHandler líneas 7-13

--- src/main/java/com/emmanuelito18_code/Handlers/CancelandStopIntentHandler.java
+++ src/main/java/com/emmanuelito18_code/Handlers/CancelandStopIntentHandler.java
@@ -4,6 +4,13 @@
  */
 package com.emmanuelito18_code.Handlers;
 
+//librerias de amazon alexa
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+import static com.amazon.ask.request.Predicates.intentName;
+
+import java.util.Optional;
 /**
  * @author Emmanuelito18
  */
