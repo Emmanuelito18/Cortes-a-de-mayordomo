9d1cb48357a0a70b78042af619986b15ccf0fa90
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-17 20:47:57 -0600

    -Cambios en paquete com.emmanuelito18_code.cortesiaDeMayordomo *Se renombró el paquete de com.emmanuelito18_code.cortesiaDeMayordomo a com.emmanuelito18_code.mayordomoServicial -Cambios en CortesiaDeMayordomo.java *Se cambio el nombre de la clase principal CortesiaDeMayordomo.java a MayordomoServicial.java

--- src/main/java/com/emmanuelito18_code/mayordomoServicial/MayordomoServicial.java
+++ src/main/java/com/emmanuelito18_code/mayordomoServicial/MayordomoServicial.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.emmanuelito18_code.cortesiademayordomo;
+package com.emmanuelito18_code.mayordomoServicial;
 
 /**
  * @author Emmanuelito18
  */
-public class CortesiaDeMayordomo {
+public class MayordomoServicial {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
