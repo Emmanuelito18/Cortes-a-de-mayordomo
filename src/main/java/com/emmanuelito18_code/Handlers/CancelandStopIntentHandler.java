8652dd9aa3ff13a634d50e21b475fb1ac8d00428
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-18 20:20:14 -0600

    -Cambios en CancelandStopIntenHandler.java *Se cambió el mensaje de despedida línea 26

--- src/main/java/com/emmanuelito18_code/Handlers/CancelandStopIntentHandler.java
+++ src/main/java/com/emmanuelito18_code/Handlers/CancelandStopIntentHandler.java
@@ -23,7 +23,7 @@ public class CancelandStopIntentHandler implements RequestHandler{
     @Override
     public Optional<Response> handle(HandlerInput input){
         return input.getResponseBuilder().withSpeech("Hasta la próxima viejos sabrosos")
-                .withSimpleCard("Mayordomo servicial","Hasta la próxima viejos sabrosos")
+                .withSimpleCard("Mayordomo servicial","Fue un placer servirle")
                 .withShouldEndSession(true).build();
     }
 }
