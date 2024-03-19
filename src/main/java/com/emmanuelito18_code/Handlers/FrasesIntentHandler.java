9b4b0d2149acec99188a6a0add6b4608eba37a83
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-18 21:04:38 -0600

    -Cambios en paquete com.emmanuelito18_code.Handlers *Se añadió la clases FrasesIntentHandler.java #sirve para que alexa diga las frases de mayordomo cuando la skill sea invocada

---
+++ src/main/java/com/emmanuelito18_code/Handlers/FrasesIntentHandler.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.emmanuelito18_code.Handlers;
+
+/**
+ * @author Emmanuelito18
+ */
+public class FrasesIntentHandler {
+    
+}
